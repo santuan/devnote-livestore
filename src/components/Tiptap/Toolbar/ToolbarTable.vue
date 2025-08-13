e718a79bf7070d89efbae5425ac2aad71b274cde
santuan <smrcouto88@gmail.com>
2025-08-12 22:48:19 -0300

    bump up + finish table v1

--- src/components/Tiptap/Toolbar/ToolbarTable.vue
+++ src/components/Tiptap/Toolbar/ToolbarTable.vue
@@ -14,8 +14,11 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <div class="gap-1 flex-wrap w-full grid border border-secondary p-1">
-    <div class="flex pb-2 justify-between items-center w-full gap-2">
+  <div class="gap-1 @xs:grid-cols-4 pb-3  w-full grid border border-secondary p-1">
+    <div class="flex pb-2 @xs:col-span-4 justify-between items-center w-full gap-2">
+      <p class="text-left justify-start items-center w-full flex text-xs">
+        <span>Table</span>
+      </p>
       <ToolbarButton
         class="cursor-default flex items-center @xs:min-w-24! border border-secondary px-2 justify-center h-7 gap-2 outline-hidden focus-visible:bg-primary/30 text-xs! hover:bg-primary/24"
         @click="
@@ -30,23 +33,21 @@ const { t } = useI18n()
         <Plus class="size-4" />
         <span class="@xs:inline-flex hidden">Add</span>
       </ToolbarButton>
-      <p class="text-center justify-center items-center w-full flex text-xs">
-        <span>Table</span>
-      </p>
+
       <ToolbarButton
         class="cursor-default flex items-center @xs:min-w-24! border border-secondary px-2 justify-center h-7 gap-2 outline-hidden focus-visible:bg-primary/30 text-xs! hover:bg-primary/20"
         :disabled="!editor.can().deleteTable()"
+        :class="editor.can().deleteTable() ? '' : 'opacity-30 pointer-events-none'"
         @click="editor.chain().focus().deleteTable().run()"
       >
         <span class="sr-only">{{ t("toolbar.deleteTable") }}</span>
         <Minus class="size-4" />
-
         <span class="@xs:inline-flex hidden">Delete</span>
       </ToolbarButton>
     </div>
     <div
-      class="grid grid-cols-3 mt-2"
-      :class="editor.can().deleteTable() ? '' : 'opacity-10'"
+      class="grid @xs:col-span-2 grid-cols-3"
+      :class="editor.can().deleteTable() ? '' : 'opacity-30 pointer-events-none'"
     >
       <div />
       <div class="flex justify-center items-center">
@@ -73,7 +74,7 @@ const { t } = useI18n()
       <div
         class="flex justify-center h-20 border-1 border-secondary items-center"
       >
-        <Grid3x2 class="size-12!" :stroke-width="0.3" />
+        <Grid3x2 class="size-10!" :stroke-width="0.3" />
       </div>
       <div class="flex justify-start items-center">
         <ToolbarButton
@@ -97,40 +98,40 @@ const { t } = useI18n()
         </ToolbarButton>
       </div>
       <div />
-
-      <div
-        class="bg-secondary col-span-3 border border-secondary w-full mt-2 gap-px grid @sm:grid-cols-4 grid-cols-2"
+    </div>
+    <div
+      :class="editor.can().deleteTable() ? '' : 'opacity-30 pointer-events-none'"
+      class="bg-secondary @xs:col-span-2 border border-secondary w-full mt-2 gap-px grid"
+    >
+      <ToolbarButton
+        class="cursor-default bg-background text-pretty px-1 py-2 outline-hidden focus-visible:bg-primary/30 text-center hover:bg-primary/20"
+        :disabled="!editor.can().mergeCells()"
+        @click="editor.chain().focus().mergeCells().run()"
       >
-        <ToolbarButton
-          class="cursor-default bg-background text-pretty px-1 py-2 outline-hidden focus-visible:bg-primary/30 text-center hover:bg-primary/20"
-          :disabled="!editor.can().deleteColumn()"
-          @click="editor.chain().focus().deleteColumn().run()"
-        >
-          {{ t("toolbar.deleteColumn") }}
-        </ToolbarButton>
-        <ToolbarButton
-          class="cursor-default bg-background text-pretty px-1 py-2 outline-hidden focus-visible:bg-primary/30 text-center hover:bg-primary/20"
-          :disabled="!editor.can().deleteRow()"
-          @click="editor.chain().focus().deleteRow().run()"
-        >
-          {{ t("toolbar.deleteRow") }}
-        </ToolbarButton>
+        {{ t("toolbar.mergeCells") }}
+      </ToolbarButton>
+      <ToolbarButton
+        class="cursor-default bg-background text-pretty px-1 py-2 outline-hidden focus-visible:bg-primary/30 text-center hover:bg-primary/20"
+        :disabled="!editor.can().splitCell()"
+        @click="editor.chain().focus().splitCell().run()"
+      >
+        {{ t("toolbar.splitCell") }}
+      </ToolbarButton>
 
-        <ToolbarButton
-          class="cursor-default bg-background text-pretty px-1 py-2 outline-hidden focus-visible:bg-primary/30 text-center hover:bg-primary/20"
-          :disabled="!editor.can().mergeCells()"
-          @click="editor.chain().focus().mergeCells().run()"
-        >
-          {{ t("toolbar.mergeCells") }}
-        </ToolbarButton>
-        <ToolbarButton
-          class="cursor-default bg-background text-pretty px-1 py-2 outline-hidden focus-visible:bg-primary/30 text-center hover:bg-primary/20"
-          :disabled="!editor.can().splitCell()"
-          @click="editor.chain().focus().splitCell().run()"
-        >
-          {{ t("toolbar.splitCell") }}
-        </ToolbarButton>
-      </div>
+      <ToolbarButton
+        class="cursor-default bg-background text-pretty px-1 py-2 outline-hidden focus-visible:bg-primary/30 text-center hover:bg-primary/20"
+        :disabled="!editor.can().deleteColumn()"
+        @click="editor.chain().focus().deleteColumn().run()"
+      >
+        {{ t("toolbar.deleteColumn") }}
+      </ToolbarButton>
+      <ToolbarButton
+        class="cursor-default bg-background text-pretty px-1 py-2 outline-hidden focus-visible:bg-primary/30 text-center hover:bg-primary/20"
+        :disabled="!editor.can().deleteRow()"
+        @click="editor.chain().focus().deleteRow().run()"
+      >
+        {{ t("toolbar.deleteRow") }}
+      </ToolbarButton>
     </div>
   </div>
 </template>
